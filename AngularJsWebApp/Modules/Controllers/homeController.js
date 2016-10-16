bdb2bb164cf48c65d0ef53b2bb7593bbdc4683f5
Deepak karma <deepakkarma@gmail.com>
2016-10-16 23:41:03 +0530

    First commit

---
+++ AngularJsWebApp/Modules/Controllers/homeController.js
@@ -0,0 +1,11 @@
+﻿(function () {
+'use strict';
+    app.controller('homeController', ['$scope','$http', function ($scope,$http) {
+        $http.get(serviceBase + "api/Deepak/5").then(function (d) {
+        $scope.TestCall = d.data;
+    }, function (error) {
+        console.log('Oops! Something went wrong while fetching the data.')
+        console.log(error)
+    });
+}]);
+})();
---
+++ WebAPI/Controller/DeepakController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controller
+{
+    public class DeepakController : ApiController
+    {
+        // GET: api/Deepak
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Deepak/5
+        public string Get(int id)
+        {
+            return "DK Don";
+        }
+
+        // POST: api/Deepak
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Deepak/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Deepak/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
